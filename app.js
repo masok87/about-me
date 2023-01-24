aea02905ac379f225f8449e37822988ec6e96973
masok87 <mdanca1987@gmail.com>
2023-01-24 10:59:21 +0000

    ade few changes in app and also in style

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 "use strict";
 
+
+
+
 let question1 = confirm("You ready to RUUUUMBLE!?");
 
 if (question1 == true) {
-  console.log("LET'S GET READY TO RUMBLLLLLLLE!");
+  alert("LET'S GET READY TO RUMBLLLLLLLE!");
 } else {
-  console.log("Ah well.");
+  alert("Ah well.");
 }
 
 let colour = prompt("What is your favourite colour?");
 
-
-
 switch (colour.toLowerCase()) {
   case "red":
-    console.log("Your favourite colour was red. Gross.");
+    alert("Your favourite colour was red. Gross.");
     break;
   case "green":
-    console.log("Nice. Good choice.");
+    alert("Nice. Good choice.");
     break;
   default:
-    console.log("Pretty sure that colour doesn't exist. Have you thought about red or green? ¯\\_(ツ)_/¯");
+    alert("Pretty sure that colour doesn't exist. Have you thought about red or green? ¯\\_(ツ)_/¯");
 }
+let question2 = confirm("Do you like football?");
+if (question2 == true) {
+  alert("Let`s go and play !");} else {
+    alert("Wasn`t expecting that!!! ")
+  }
+  let question3 = confirm("Do you like  Liverpool Fc ?"); if (question3 == true) {
+    alert("Up the Reds!!!");
+  } else {
+    alert("Sad to hear that ");
+  }
+  let question4 = confirm("Have you ever been on Anfield ?"); if (question4 == true ) { 
+    alert("Awesome!!!");
+  } else {
+    alert("You should go at least once !");
+  }
