02f8c2f1adbc58d80ec68103a6f76157a8582b70
masok87 <mdanca1987@gmail.com>
2023-01-29 17:20:08 +0000

    finished lab 03

--- app.js
+++ app.js
@@ -1,52 +1,127 @@
-"use strict";
+let userPoints = 0;
 
+alert("Hi , welcome to my guessing game .");
 
+let user = prompt("What is your name ?");
 
+// ensure the user ACTUALLY gives their name
+while (!user) {
+  user = prompt("It will be really great if i can have your name .");
+}
 
-let question1 = confirm("Do you want to take part in a small quizz?");
-
-if (question1 == true) {
-  alert("Let`s get down to bussines");
+// lament at Ronald
+if (user == "John" || user == "john") {
+  alert("uhh , I knew it was you ");
 } else {
-  alert("Ah well.");
+  alert("Hello there " + user + "! I'm so glad you decided to join me!");
 }
 
-let colour = prompt("What is my  favourite colour?");
+// question one
+function foodQuestion() {
+  let Question1 = prompt("What is one my favourite foods ?.").toLowerCase();
 
-switch (colour.toLowerCase()) {
-  case "red":
-    alert("My  favourite colour is  red. Great.");
-    break;
-  case "green":
-    alert("Nice. But i prefer red ");
-    break;
-  default:
-    alert("Pretty sure that colour doesn't exist. Have you thought about red ? ");
+  let foodILike = ["pizza ", "steak", "chinese", "soup"];
+
+  for (let i = 0; i < foodILike.length; i++) {
+    if (Question1 == foodILike[i]) {
+      alert("Yes I love " + Question1 + "!");
+      userPoints++;
+    }
+  }
 }
-let question2 = confirm("What do you think , do i like football?");
-if (question2 == true) {
-  alert("Great , you get to know me!");} else {
-    alert("Wasn`t expecting that!!! ")
-  }
-  let question3 = confirm("I live in Liverpool , what do you think , do i follow  Liverpool Fc ?"); if (question3 == true) {
-    alert("Up the Reds!!!");
-  } else {
-    alert("Ohhh noo , i`m a big fan of them  ");
-  }
-  let question4 = confirm("Have you ever been on Anfield ?"); if (question4 == true ) { 
-    alert("Awesome!!!");
-  } else {
-    alert("You should go at least once !");
-  }
-  let question5 = confirm("Do you wanna play a mind game?");if (question4 == true ) { 
-    alert("Good stuff !!!");
-  } else {
-    alert("You should give it a go !");
-  }
-  
-  let question6 = prompt("I`m thinking of a number between 1 and 100 , can you guess that from 4 attempts?You cand start now with your first attempt !");
-  let myNumber = 25 ;
-  
-  
 
-  
+// question two
+function footballQuestion() {
+  let Question2 = confirm("What do you think ,do I support Liverpool?");
+
+  if (Question2 == true) {
+    alert("Amazing!.");
+    userPoints++;
+  } else {
+    alert("Common ...");
+  }
+}
+function stadiumQuestion() {
+  let Question3 = confirm("Do i go the matches  ?");
+
+  if (Question3 == true) {
+    alert("Indeed.");
+    userPoints++;
+  } else {
+    alert("I tought you started to know me .");
+  }
+}
+function musicQuestion() {
+  let Question4 = prompt(
+    "What is one my favourite music bands?."
+  ).toLowerCase();
+
+  let bandsIlike = ["Keane", "Cold Play ", "U2"];
+
+  for (let i = 0; i < bandsIlike.length; i++) {
+    if (Question4 == bandsIlike[i]) {
+      alert(Question4 + " is peak.");
+      userPoints++;
+    }
+  }
+}
+function danceQuestion() {
+  let Question5 = confirm("What do you think ,  do i like traveling? ");
+
+  if (Question5 == true) {
+    alert("Brilliant ! ");
+    userPoints++;
+  } else {
+    alert("ohh noo");
+  }
+}
+function guessQuestion() {
+  let guessQuestion = prompt(
+    "what number between 10 and 30 am i thinking at, you can have 4 guesses . "
+  );
+  let attempts = 4;
+  while (attempts > 0) {
+    // if incorrect
+    if (guessQuestion > 13) {
+      alert("try lower " + attempts + " attempts left ");
+      --attempts;
+      guessQuestion = prompt("what number between 10 and 30 am i thinking of?");
+    } else if (guessQuestion < 13) {
+      alert("try a higher number " + attempts + " attempts left ");
+      --attempts;
+      guessQuestion = prompt("what number between 10 and 30 am i thinking of?");
+    } else if (guessQuestion == 13) {
+      // if correct
+      alert("nice, thats my number ");
+      attempts = 0;
+    }
+  }
+}
+function guessQ() {
+  let question = prompt("What sport do I like ?");
+
+  let answers = ["football", "pool", "tennis", "ping pong", "basketball"];
+  let attempts = 5;
+
+  while (attempts > 0) {
+    for (let i = 0; i < answers.length; i++) {
+      if (question == answers[i]) {
+        alert("Great, that`s a great sport ");
+        attempts = 0;
+      }
+    }
+    if (attempts > 0) {
+      question = prompt("I don`t really like that sport.");
+      attempts--;
+    }
+  }
+}
+
+guessQuestion();
+guessQ();
+foodQuestion();
+footballQuestion();
+stadiumQuestion();
+musicQuestion();
+danceQuestion();
+alert("Well done " + user + "! You scored " + userPoints + "/7");
